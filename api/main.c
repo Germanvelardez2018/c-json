8974c28258285254fe113d64c41ad5375c304d5b
German Velardez <germanvelardez16@gmail.com>
2021-10-18 08:32:18 -0300

    init project

---
+++ api/main.c
@@ -0,0 +1,21 @@
+/**Date: 18/10/2021 
+ * Author: German Velardez
+ * 
+ * 
+ * Description: 
+ * Handling strings with json format in c language
+ * 
+ * **/
+
+
+#include <stdio.h>
+
+
+
+
+
+int main(void)
+{
+    printf("hello word\n");
+     return 0;
+}
